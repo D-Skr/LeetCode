33a994224c6210a8b8a03f1c5f3420222fbec1ef
Di <altusj@gmail.com>
2022-08-29 11:50:34 -0400

    add in-place solution

--- arrays/RevertArray.java
+++ arrays/RevertArray.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class RevertArray {
 
+	//v.1 solution with loop and new array
 	public static int[] reverse(int[] arr) {
 		int[] result = new int[arr.length];
 		
@@ -13,12 +14,33 @@ public class RevertArray {
 		return result;
 	}
 	
+	//v.2 in-place solution
+	public static void reverseInPlace(int[] arr) {
+		for(int i = 0; i < arr.length / 2; i++) {
+			//swap item at index i and item at index (arr.length -1 -i)
+			int temp = arr[i];
+			arr[i] = arr[arr.length -1 -i];
+			arr[arr.length -1 -i] = temp;
+		}
+	}
+	
 	public static void main(String[] args) {
-		int[] arr = {0,2,3,4,5,6,1,1};
+		int[] arr1 = {0,2,3,4,5,6,1,1};
+		int[] arr2 = {0,2,3,4,5,6,1,1};
 		
-		Arrays.stream(reverse(arr))
+		Arrays.stream(reverse(arr1))
 			.forEach(System.out::println);
-
+		
+		System.out.println("~~~~~~~~~");
+		reverseInPlace(arr2);
+		Arrays.stream(arr2)
+		.forEach(System.out::println);
+		
+		System.out.println("~~~~~~~~~");
+		//return back reversed array
+		reverseInPlace(arr2);
+		Arrays.stream(arr2)
+		.forEach(System.out::println);
 	}
 
 }
