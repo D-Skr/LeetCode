c586a48b5c7b7287eb724efdf56cde1d9881fc9e
Di <altusj@gmail.com>
2022-09-06 14:10:08 -0400

    add ListNode class

--- LinkedList/Q206ReverseLinkedList.java
+++ LinkedList/Q206ReverseLinkedList.java
@@ -1,9 +1,29 @@
 package LinkedList;
 
 public class Q206ReverseLinkedList {
-
+	/**
+	 * Definition for singly-linked list.
+	 * public class ListNode {
+	 *     int val;
+	 *     ListNode next;
+	 *     ListNode() {}
+	 *     ListNode(int val) { this.val = val; }
+	 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	 * }
+	 */
+	    public static ListNode reverseList(ListNode head) {
+	        ListNode current = head;
+	        ListNode prev = null;
+	        ListNode temp = null;
+	        while(current != null){
+	            temp = current.next;
+	            current.next = prev;
+	            prev = current;
+	            current = temp;
+	        }        
+	        return prev;
+	    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
