3f1a75eaf4977449acff6d2899623a15acaba734
Di <altusj@gmail.com>
2022-10-23 21:44:29 -0400

    add cipher with Exclusive OR

--- Bits/CipherExclusiveOR.java
+++ Bits/CipherExclusiveOR.java
@@ -1,6 +1,17 @@
 package Bits;
 
 public class CipherExclusiveOR {
+	
+	String msg;
+	String encmsg;
+	int key;
+	
+	String encode(String msg, int key){
+		for(int i = 0; i < msg.length(); i++) {
+			
+		}
+		return encmsg;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
