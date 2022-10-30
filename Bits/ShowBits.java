94f860ad78f76e787d96987da956f582a6ef6bd1
Di <altusj@gmail.com>
2022-10-29 23:23:00 -0400

    add solution for show bits

--- Bits/ShowBits.java
+++ Bits/ShowBits.java
@@ -8,8 +8,8 @@ public class ShowBits {
 
         val = 123;
         for (t = 128; t > 0; t  = t/2) {
-            if ((val & t) != 0) System.out.println("1 ");
-            
+            if ((val & t) != 0) System.out.print("1 ");
+            else System.out.print("0 ");
         }
     }
 
