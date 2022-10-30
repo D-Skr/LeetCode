8051d222c2b79bec617ca4bc733910bdd2bfc5ba
Di <altusj@gmail.com>
2022-10-29 23:28:03 -0400

    add solution for show bits

--- Bits/ShowBits.java
+++ Bits/ShowBits.java
@@ -3,18 +3,15 @@ package Bits;
 public class ShowBits {
     // show bits of byte
     static void showBits(byte val) {
-        
-    }
-    
-    public static void main(String[] args) {
-        int t;
-        byte val;
-
-        val = -128;
-        for (t = 128; t > 0; t  = t/2) {
+        for (int t = 128; t > 0; t  = t/2) {
             if ((val & t) != 0) System.out.print("1 ");
             else System.out.print("0 ");
         }
+    }
+    
+    public static void main(String[] args) {
+        byte val = -128;
+        showBits(val);
     }
 
 }
