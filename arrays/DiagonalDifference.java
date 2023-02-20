7028c08d0c6dc27c081f4706a51e54642c55a1e1
Dmitriy <altusj@gmail.com>
2023-02-19 21:56:41 -0500

    add new problem

--- arrays/DiagonalDifference.java
+++ arrays/DiagonalDifference.java
@@ -1,6 +1,12 @@
 package arrays;
 
+import java.util.List;
+
 public class DiagonalDifference {
+    
+    static int diagonalDifference(List<List<Integer>> arr) {
+        return -1;
+    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
