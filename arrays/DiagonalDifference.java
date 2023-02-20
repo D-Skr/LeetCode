37c8004bb14a4d5ae1ff59b116280a2cf62e10e6
Dmitriy <altusj@gmail.com>
2023-02-19 21:58:25 -0500

    add description

--- arrays/DiagonalDifference.java
+++ arrays/DiagonalDifference.java
@@ -1,6 +1,12 @@
 package arrays;
 
 import java.util.List;
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
 
 public class DiagonalDifference {
     
