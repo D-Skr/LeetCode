1a278f7f69132ce934f0763e65c8f5aa986894e5
Dmitriy <altusj@gmail.com>
2023-02-21 23:19:44 -0500

    attempt to solve

--- sort/CountingSort.java
+++ sort/CountingSort.java
@@ -9,26 +9,42 @@ public class CountingSort {
     
     public static List<Integer> countingSort(List<Integer> arr) {
         // Write your code here
+        if (arr == null || arr.isEmpty()) {
+            return arr;
+        }
             int maxValue = Collections.max(arr);
             Integer[] count = new Integer[maxValue+1];
-            List<Integer> result = new ArrayList<>();
-            //Collections.fill(result,0);
+            Integer[] out = new Integer[maxValue+1];
+            List<Integer> result = new ArrayList<>(arr.size());
+            Collections.fill(result,0);
             for(int i : arr){
                 count[i]++;
+                //result.set(i+1, result.get(i)+1);
+                
                 //System.out.println(countArr[i]);
                 // System.out.print(result.get(i) + " ");
                 // result.set(i, result.get(i)+1);
                 // System.out.println(result.get(i));
             }
-            //System.out.println(Arrays.deepToString(count));
-            result = Arrays.asList(count);
+            System.out.println(Arrays.deepToString(count));
+            for(int i = 1; i < count.length; i++) {
+                count[i] += count[i - 1];
+            }
+            
+            for(int i = 1; i <= count.length; i++) {
+                //result.set(count[arr.get(i)] - 1, i);
+                out[count[arr.get(i)] -1] = arr.get(i);
+                count[arr.get(i)]--;
+            }
+            
+            result = Arrays.asList(out);
             return result;
         }
 
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        List<Integer> list = Arrays.asList(1,1,3,2,1);
+        System.out.println(countingSort(list));
     }
 
 }
