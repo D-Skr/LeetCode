71f3a1a8ce6be5c009d387ea851bed31ddcbe7f9
Dmitriy <altusj@gmail.com>
2023-04-12 14:11:57 -0400

    missing number in array solution

--- arrays/MissingNumber.java
+++ arrays/MissingNumber.java
@@ -2,7 +2,13 @@ package arrays;
 
 public class MissingNumber {
     //find the missing number in an integer array of 1 to 10
-
+    
+    public void missingNumber(int[] intArray) {
+        for(int i = 0; i < intArray.length - 1; i++) {
+            
+        }
+    }
+    
     public static void main(String[] args) {
         
     }
