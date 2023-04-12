b5ae4c8e78d81630d5a56c810834d901a932e7e0
Dmitriy <altusj@gmail.com>
2023-04-12 14:10:18 -0400

    missing number in array

--- arrays/MissingNumber.java
+++ arrays/MissingNumber.java
@@ -1,10 +1,10 @@
 package arrays;
 
 public class MissingNumber {
+    //find the missing number in an integer array of 1 to 10
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        
     }
 
 }
