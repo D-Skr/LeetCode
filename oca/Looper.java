2499f64442edd5585cfb881ee0a9490ee43c41ec
Dmitriy <altusj@gmail.com>
2023-07-15 21:38:46 -0400

    add looper problem

--- oca/Looper.java
+++ oca/Looper.java
@@ -16,6 +16,6 @@ public class Looper {
             System.out.print("kk = " + kk + ", ");
             kk = kk + 1;
         }
-        System.out.print("kk = " + kk + ", ");
+        System.out.print("kk = " + kk + ", "); //local var kk invisible outside block.
     }
 }
