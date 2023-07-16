d986bf4d90f744c1ffb854ad0eb2a7e831d9f6fe
Dmitriy <altusj@gmail.com>
2023-07-15 21:36:22 -0400

    add looper problem

--- oca/Looper.java
+++ oca/Looper.java
@@ -1,12 +1,14 @@
 package oca;
 
+
+
 public class Looper {
 
     public static void main(String[] args) {
         for(int kk = 0; kk < 4; kk++) {
-            
+            System.out.print("kk = " + kk + ", ");
+            kk = kk + 1;
         }
-
+        System.out.print("kk = " + kk + ", ");
     }
-
 }
