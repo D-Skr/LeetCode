942cb8cebc19105157d918048140023fef16708c
Dmitriy <altusj@gmail.com>
2023-07-15 21:48:29 -0400

    add test of Java name spaces

---
+++ oca/String.java
@@ -0,0 +1,13 @@
+package oca;
+
+/*
+ * Java has 6 name spaces.
+ */
+public class String {
+
+    public static void main(java.lang.String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
