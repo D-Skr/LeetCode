3f565db71f50b06df3cf250000ca7caf08ae6068
Dmitriy <altusj@gmail.com>
2023-07-17 17:09:01 -0400

    data type problem

--- oca/DataTypesOne.java
+++ oca/DataTypesOne.java
@@ -2,6 +2,10 @@ package oca;
 
 /* 
  * what is the result?
+ * 21.0 32.0 107.0 43.0
+ * 21.0 32.0 a10 43.0
+ * Compilation fails
+ * An exception is thrown at run time
  */
 public class DataTypesOne {
 
