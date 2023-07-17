6588819f686d2145ad23945a1f84c87218917cf6
Dmitriy <altusj@gmail.com>
2023-07-17 18:52:30 -0400

    data type problem

---
+++ oca/DataTypesTwo.java
@@ -0,0 +1,10 @@
+package oca;
+
+public class DataTypesTwo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ oca/Engine.java
@@ -0,0 +1,10 @@
+package oca;
+
+public class Engine {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
