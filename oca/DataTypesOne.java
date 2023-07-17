19ef0f76765c0f233c0aab4f2a053585dcc04704
Dmitriy <altusj@gmail.com>
2023-07-17 17:07:46 -0400

    data type problem

--- oca/DataTypesOne.java
+++ oca/DataTypesOne.java
@@ -8,7 +8,8 @@ public class DataTypesOne {
     public static void main(String[] args) {
         double[] darr = {11,22.0,'a',33.0f};
         for (double e : darr) {
-            
+            e = e + 10;
+            System.out.println(e + " ");
         }
 
     }
