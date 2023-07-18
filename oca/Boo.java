eded16644e72960b20f88652c349b3080bfab269
Dmitriy <altusj@gmail.com>
2023-07-17 20:51:29 -0400

    Add operator problem

--- oca/Boo.java
+++ oca/Boo.java
@@ -5,7 +5,7 @@ public class Boo {
     public static void main(String[] args) {
         int a,b = 0;
         boolean boo;
-        a = (boo = true) ? b = 10 : 11;
+        a = (boo = true) ? b = 10 : 11; // b=10 -> a = b;
         System.out.println(a + " " + boo + " " + b); //10 true 10
 
     }
