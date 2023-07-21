6120951ffd052718b03f0ed6ad45f528f0f99fa5
Dmitriy <altusj@gmail.com>
2023-07-21 11:34:39 -0400

    Add postfix problem

--- oca/PostFix.java
+++ oca/PostFix.java
@@ -2,6 +2,14 @@ package oca;
 
 public class PostFix {
 
+    static int incr1(int x) {
+        return x++;
+    }
+    
+    static int incr2(int x) {
+        x++;
+        return x;
+    }
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
