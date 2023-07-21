4f05f3adc010b26167dba8c8e3917e2ba48cf060
Dmitriy <altusj@gmail.com>
2023-07-21 11:36:27 -0400

    Add operator problem

--- oca/PostFix.java
+++ oca/PostFix.java
@@ -13,7 +13,15 @@ public class PostFix {
     public static void main(String[] args) {
         int a, b, c;
         a = b = c = 0;
+        
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(incr1(b));
+        System.out.println(incr2(c));
 
+        System.out.println(incr1(b));
+        System.out.println(incr1(b));
+        System.out.println(incr1(b));
     }
 
 }
