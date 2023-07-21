20174a7c54dcb308739798b1195db73587d14dd0
Dmitriy <altusj@gmail.com>
2023-07-21 11:50:55 -0400

    Add concat problem

---
+++ oca/AddString.java
@@ -0,0 +1,17 @@
+package oca;
+
+public class AddString {
+
+    public static void main(String[] args) {
+        System.out.println("1 + 2 = " + 3 + 4);
+        System.out.println("1 + 2 = " + (3) + (4));
+        System.out.println("1 + 2 = " + (3 + 4));
+
+        /*
+         * 1 + 2 = 34
+         * 1 + 2 = 34
+         * 1 + 2 = 7
+         */
+    }
+
+}
