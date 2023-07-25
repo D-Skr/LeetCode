d4888ae3c9d91c9e3f3dca1a104e82596f5a26e2
Dmitriy <altusj@gmail.com>
2023-07-25 17:46:38 -0400

    add String Equality problem

--- oca/TestEquality.java
+++ oca/TestEquality.java
@@ -6,7 +6,10 @@ public class TestEquality {
         String str1 = "Hello";
         String str2 = "Hello";
         String str3 = new String("Hello");
-
+        
+        System.out.println(str1 == str2);   
+        System.out.println(str1 == str3);
+        System.out.println(str1.equals(str2));
     }
 
 }
