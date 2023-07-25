9223b282df2d97b41c3d79cb84a51ab14cb03258
Dmitriy <altusj@gmail.com>
2023-07-25 17:45:07 -0400

    add String Equality problem

--- oca/TestEquality.java
+++ oca/TestEquality.java
@@ -3,7 +3,9 @@ package oca;
 public class TestEquality {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = new String("Hello");
 
     }
 
