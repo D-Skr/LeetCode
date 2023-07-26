4575d935609fa3bfbd29c2019d0f2a57564d3d21
Dmitriy <altusj@gmail.com>
2023-07-25 20:08:23 -0400

    add IfElse problem

--- oca/IfElse.java
+++ oca/IfElse.java
@@ -4,7 +4,13 @@ public class IfElse {
 
     public static void main(String[] args) {
         //if(boolean bb = true) System.out.println(bb); //comp error
-
+        
+        boolean b;
+        Boolean boo;
+        Boolean booo;
+        if (b = true) System.out.println(b);
+        if (boo = Boolean.TRUE) System.out.println(boo);
+        if (booo = true) System.out.println(booo);
     }
 
 }
