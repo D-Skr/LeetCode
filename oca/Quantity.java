749f6a92580060c982a00ca907b3724c7c8af54e
Dmitriy <altusj@gmail.com>
2023-07-26 13:12:03 -0400

    add Quantity problem

--- oca/Quantity.java
+++ oca/Quantity.java
@@ -1,15 +1,14 @@
 package oca;
 
 public class Quantity {
-    private boolean flag;
+    private boolean flag; //dafault value 'false'
     public void show() {
         int qty = flag ? 1 : 100;
-        System.out.println("Hom many " + qty++);
+        System.out.println("Hom many " + qty++); //100, post increment
     }
     
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        new Quantity().show();
     }
 
 }
