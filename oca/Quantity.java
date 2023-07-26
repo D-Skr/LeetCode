4bbcadfbfc3ba609be2fce9f0ad27e2cb921bb57
Dmitriy <altusj@gmail.com>
2023-07-26 13:10:07 -0400

    add Quantity problem

--- oca/Quantity.java
+++ oca/Quantity.java
@@ -3,7 +3,8 @@ package oca;
 public class Quantity {
     private boolean flag;
     public void show() {
-        
+        int qty = flag ? 1 : 100;
+        System.out.println("Hom many " + qty++);
     }
     
     public static void main(String[] args) {
