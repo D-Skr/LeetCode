6f9c75fee820bd384b495424f3422567dd7330bf
Dmitriy <altusj@gmail.com>
2023-07-26 13:09:13 -0400

    add Quantity problem

--- oca/Quantity.java
+++ oca/Quantity.java
@@ -2,7 +2,9 @@ package oca;
 
 public class Quantity {
     private boolean flag;
-    
+    public void show() {
+        
+    }
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
