21314f9240bdfd8afdece3eee3739db485f9f846
Dmitriy <altusj@gmail.com>
2023-07-25 21:16:32 -0400

    add IfElse problem

--- oca/IfElse.java
+++ oca/IfElse.java
@@ -16,11 +16,14 @@ public class IfElse {
         System.out.println("if with label");
         
         //while(false) System.out.println("comp error - unreachable code");
+        if(false) System.out.println("works fine");
         //while(!true) System.out.println("comp error - unreachable code");
+        if(!true) System.out.println("works fine");
         b = false;
         while(b) System.out.println("works fine");
         final boolean bf = false;
         //while(bf) System.out.println("comp error - unreachable code");
+        if(bf) System.out.println("comp error - unreachable code");
     }
 
 }
