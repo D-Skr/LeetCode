2ed4be638bf7878546b40a7763fb25aaeedb3ed6
Dmitriy <altusj@gmail.com>
2023-07-26 17:45:00 -0400

    add Switch problem

---
+++ oca/SwitchTest.java
@@ -0,0 +1,16 @@
+package oca;
+
+public class SwitchTest {
+
+    public static void main(String[] args) {
+       
+        byte a = 10;
+        
+        switch(a) {
+        case 10:
+            System.out.println("!");
+        }
+
+    }
+
+}
