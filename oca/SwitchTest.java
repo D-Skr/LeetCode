69f5a17682bda64c1a20fd30430443cccd1efcd7
Dmitriy <altusj@gmail.com>
2023-07-26 17:52:41 -0400

    add Switch cases

--- oca/SwitchTest.java
+++ oca/SwitchTest.java
@@ -5,7 +5,7 @@ public class SwitchTest {
     public static void main(String[] args) {
        
         byte a = 10;
-        
+        //v1
         switch(a) {
         case 10:
             System.out.println("!");
@@ -16,12 +16,21 @@ public class SwitchTest {
 //            System.out.println("!");     
         }
         
+        //v2
         char ch = 'a';
         switch(ch) {        //works fine
         case 10:
             System.out.println("!");
         case 128:       //works fine
             System.out.println("!"); 
+        }
+        
+        //v3
+        int i1 = 10;
+        int i2 = 20;
+        switch(i1) {        
+        case i2:        //Comp err. Case expressions must be a constant expressions
+            System.out.println("!");
         }
     }
 
