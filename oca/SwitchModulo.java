0262c70d2b0c4149661e11615f18b77ff2c7ef90
Dmitriy <altusj@gmail.com>
2023-07-26 21:17:24 -0400

    add SwitchModulo problem

--- oca/SwitchModulo.java
+++ oca/SwitchModulo.java
@@ -4,8 +4,16 @@ public class SwitchModulo {
 
     public static void main(String[] args) {
         int a = 10;
-        Short s = (short)(int)(long)(float)(a % 4.d);
-
+        Short s = (short)(int)(long)(float)(a % 4.d); //any number of cast. Last one short-> result short too.
+        switch(s) {
+            case 0:
+                System.out.println("Divisible by 4");
+                break;
+            default: 
+                System.out.println("Not divisible by 4"); //prints
+                break;
+        }
+        System.out.println(s);
     }
 
 }
