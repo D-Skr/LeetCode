58f9bd0a77a36546f1b96198e55bb50eab45ecb0
Dmitriy <altusj@gmail.com>
2023-07-27 00:05:43 -0400

    add ArrayTest problem

---
+++ oca/ArrayTest.java
@@ -0,0 +1,35 @@
+package oca;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[3]; //arr with size 3
+        
+        //compiler counts size
+        int[] arr2 = new int[] {42,43,-3223};
+ 
+        //comperr. Cannot define dimension expressions when an array initializer is provided
+        //int[] arr3 = new int[3] {42,43,-3223}; 
+        
+        int[] arr3 = {};
+        System.out.println(arr3); // prints [I@2a139a55, 
+        //[I - 1-dimensional int array,
+        //2a139a55 - hash
+        // if [[I - 2 dimensional int array
+    }
+    /*
+     * 9 types in class descriptor:
+     * [B - byte
+     * [S - short
+     * [I - integer
+     * [J - long
+     * [F - float
+     * [D - double
+     * [C - char
+     * [Z -boolean
+     *  and
+     * [L - reftypes
+     */
+   
+}
