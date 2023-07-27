4784a8aab601ea909a7701a4e778ca9b9fcf033e
Dmitriy <altusj@gmail.com>
2023-07-26 21:28:48 -0400

    add SwitchTest problem

---
+++ oca/SwitchTest2.java
@@ -0,0 +1,14 @@
+package oca;
+//where comperr?
+public class SwitchTest2 {
+    int test;
+    int a = 10; 
+    int run() {return 42;}
+    void switchMe() {
+        switch( run() ) {}  //line1
+        switch( test ) {    //line2
+            case a:         //line3
+            default:
+        }
+    }
+}
