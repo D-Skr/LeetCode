c7a6416d9d80c849ed42445ab48113bcdbe193a9
Dmitriy <altusj@gmail.com>
2023-07-26 21:14:09 -0400

    add SwitchModulo problem

---
+++ oca/SwitchModulo.java
@@ -0,0 +1,11 @@
+package oca;
+
+public class SwitchModulo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        Short s = (short)(int)(long)(float)(a % 4.d);
+
+    }
+
+}
