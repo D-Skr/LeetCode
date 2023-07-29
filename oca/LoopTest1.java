dd093be11d64a8713b5748f7a565b5d41a12aed0
Dmitriy <altusj@gmail.com>
2023-07-28 20:21:12 -0400

    add LoopTest problem

--- oca/LoopTest1.java
+++ oca/LoopTest1.java
@@ -12,10 +12,10 @@ public class LoopTest1 {
         int i1, s1, count1 = 10;
         for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
         //v2
-        int i1, s1, count1 = 10;
-        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        int i2, s2, count2 = 10;
+        for (i2 = 0, s2 = 0; i2 < count2; s2 += i2, i2++);
         //v3
-        int i1, s1, count1 = 10;
+        int s3 = 0, count3 = 10;
         for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
         //v4
         int i1, s1, count1 = 10;
