99365e7c6c1d972cb7d6d0f85c5211b6d0bf8be8
Dmitriy <altusj@gmail.com>
2023-07-28 20:19:04 -0400

    add LoopTest problem

--- oca/LoopTest1.java
+++ oca/LoopTest1.java
@@ -6,7 +6,28 @@ public class LoopTest1 {
         //compiler treats these loops as same
         for(;;)break; 
         for(;true;) break;
+        
+        //same loop in different valid 
+        //v1
+        int i1, s1, count1 = 10;
+        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        //v2
+        int i1, s1, count1 = 10;
+        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        //v3
+        int i1, s1, count1 = 10;
+        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        //v4
+        int i1, s1, count1 = 10;
+        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        //v5
+        int i1, s1, count1 = 10;
+        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        //v6
+        int i1, s1, count1 = 10;
+        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
 
+        
     }
 
 }
