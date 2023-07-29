50b62814acacb55d47021f1ca24b0d557fc73bec
Dmitriy <altusj@gmail.com>
2023-07-28 20:24:16 -0400

    add LoopTest problem

--- oca/LoopTest1.java
+++ oca/LoopTest1.java
@@ -8,24 +8,25 @@ public class LoopTest1 {
         for(;true;) break;
         
         //same loop in different valid 
+        int count = 10;
         //v1
-        int i1, s1, count1 = 10;
-        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        int i1, s1;
+        for (i1 = 0, s1 = 0; i1 < count; i1++) { s1 += i1; }
         //v2
-        int i2, s2, count2 = 10;
-        for (i2 = 0, s2 = 0; i2 < count2; s2 += i2, i2++);
+        int i2, s2;
+        for (i2 = 0, s2 = 0; i2 < count; s2 += i2, i2++);
         //v3
-        int s3 = 0, count3 = 10;
-        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        int s3 = 0;
+        for (int i3 = 0; i3 < count; s3 += i3++);
         //v4
-        int i1, s1, count1 = 10;
-        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        int i4 = 0, s4 = 0;
+        for (i1 = 0, s1 = 0; i1 < count; i1++) { s1 += i1; }
         //v5
-        int i1, s1, count1 = 10;
-        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        int i1, s1;
+        for (i1 = 0, s1 = 0; i1 < count; i1++) { s1 += i1; }
         //v6
-        int i1, s1, count1 = 10;
-        for (i1 = 0, s1 = 0; i1 < count1; i1++) { s1 += i1; }
+        int i1, s1;
+        for (i1 = 0, s1 = 0; i1 < count; i1++) { s1 += i1; }
 
         
     }
