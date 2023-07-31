a8d5cca766a0e2a66164dfec2f990213955ce25e
Dmitriy <altusj@gmail.com>
2023-07-31 16:45:25 -0400

    add LoopTest2 problem

--- oca/LoopTest2.java
+++ oca/LoopTest2.java
@@ -6,7 +6,8 @@ public class LoopTest2 {
         
         for(int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-               
+               if( j == 1) break;
+               System.out.printf("i=%d, j=%d\n", i, j);
             }
         }
 
