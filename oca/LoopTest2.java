ea196f4181ae190e03d016cb71c6fcbdd3ed910d
Dmitriy <altusj@gmail.com>
2023-07-31 16:44:13 -0400

    add LoopTest2 problem

--- oca/LoopTest2.java
+++ oca/LoopTest2.java
@@ -3,7 +3,12 @@ package oca;
 public class LoopTest2 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+               
+            }
+        }
 
     }
 
