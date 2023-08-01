56e6c5e3078124a3e4eab6fdaac2ee728a495df7
Dmitriy <altusj@gmail.com>
2023-08-01 15:09:10 -0400

    add Overloading problem

---
+++ oca/OverloadingTest.java
@@ -0,0 +1,19 @@
+package oca;
+
+public class OverloadingTest {
+    
+    static void run(byte arg) {
+        System.out.println("byte");
+    }
+    
+    static void run(long l) {
+        System.out.println("long");
+    }
+    
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
