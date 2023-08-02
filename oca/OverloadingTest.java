ae2e14ae61a1bb2ef06af5d7ba424f786cf62af8
Dmitriy <altusj@gmail.com>
2023-08-01 23:04:49 -0400

    add Overloading problem

--- oca/OverloadingTest.java
+++ oca/OverloadingTest.java
@@ -6,18 +6,27 @@ public class OverloadingTest {
         System.out.println("byte");
     }
     
-    static void run(long l) {
-        System.out.println("long");
-    }
+//    static void run(long l) {
+//        System.out.println("long");
+//    }
     
-    static void run(Integer arg) {
-        System.out.println("Integer");
+//    static void run(Integer arg) {
+//        System.out.println("Integer");
+//    }
+    
+    static void run(int... args) {
+        System.out.println("var args");
     }
     
     public static void main(String[] args) {
 
-        run(10); //prints long
-
+        run(); 
+        /*
+         * prints
+         * 1. long
+         * 2. Integer
+         * 2. var arg
+         */
     }
 
 }
