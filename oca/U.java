f973e1d69978295c7f95d6379640f2b6b1951343
Dmitriy <altusj@gmail.com>
2023-08-03 15:30:37 -0400

    add Access modifiers problem

--- oca/U.java
+++ oca/U.java
@@ -2,10 +2,11 @@ package oca;
 
     //which LOCs fail compilation?
 
-
 abstract class A {}
 final class F {}
-public class U {}
+public class U {} //should be same as file name because of public modifier
+class N {}
+    //only abstact, final, public and w/o modifier are permitted
 protected class O {}
 private class R {}
 static class S {}
