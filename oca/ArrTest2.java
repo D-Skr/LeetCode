6a5d58cd2d7f20ef07265da8adcd42cf02d7cbb3
Dmitriy <altusj@gmail.com>
2023-08-10 12:39:40 -0400

    oca prep

--- oca/ArrTest2.java
+++ oca/ArrTest2.java
@@ -4,7 +4,7 @@ public class ArrTest2 {
 
     public static void main(String[] args) {
         Double [] arr = new Double[2];
-//        System.out.println(arr[0] + arr[1]); //Exception in thread "main" java.lang.NullPointerException
+//        System.out.println(arr[0] + arr[1]); // RTE. Exception in thread "main" java.lang.NullPointerException
         System.out.println(arr[0]); 
         //prints null
     }
