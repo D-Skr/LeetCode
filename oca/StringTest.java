afd1104532f0ab786215e23bf31437558b8d6674
Dmitriy <altusj@gmail.com>
2023-08-10 12:10:22 -0400

    oca prep

--- oca/StringTest.java
+++ oca/StringTest.java
@@ -1,9 +1,20 @@
 package oca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringTest {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        String s = new String("Hello");
+        List<String> list = new ArrayList<>();
+        list.add(s);
+        list.add(new String("Hello"));
+        list.add(s);
+        s.replace("l", "L");
+        System.out.println(list);
+        //[Hello, Hello, Hello]
+        //not [HeLLo, Hello, HeLLo]
 
     }
 
