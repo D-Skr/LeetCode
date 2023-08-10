c4a252e612a0dc0650d570624b216eec6ac1563c
Dmitriy <altusj@gmail.com>
2023-08-10 12:03:07 -0400

    oca prep

--- oca/SbTest.java
+++ oca/SbTest.java
@@ -3,8 +3,10 @@ package oca;
 public class SbTest {
 
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(100);
+        System.out.println(sb.length() + " : " + sb.toString().length());
+        System.out.println(sb.toString());
         
-
     }
 
 }
