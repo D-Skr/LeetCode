79b024a635e08bc593242df9d4aa04929ead353a
Dmitriy <altusj@gmail.com>
2023-08-11 13:16:23 -0400

    oca prep

--- oca/JavaCoder.java
+++ oca/JavaCoder.java
@@ -1,5 +1,19 @@
 package oca;
 
+interface Money {
+    int cash = 0;
+    abstract void earn(int amount);
+    public void spend(int amount);
+}
+
+class LocalCurrency implements Money{
+    int cash = 100;
+    public void earn(int amount) {
+        cash = cash + amount;
+    }
+    
+}
+
 public class JavaCoder {
 
     public static void main(String[] args) {
