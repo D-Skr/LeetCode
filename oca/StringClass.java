aa6754d79ba212e50bdab8c9838c5df855bcb995
Dmitriy <altusj@gmail.com>
2023-08-14 13:33:31 -0400

    oca prep

--- oca/StringClass.java
+++ oca/StringClass.java
@@ -6,11 +6,11 @@ public class StringClass {
         String s1 = "Hello";
         String s2 = new String("Hello");
         String s3 = new String(new StringBuilder("Hello"));
-        System.out.println("Hello" == s1);
-        System.out.println(s1 == s2);
-        System.out.println(s3 == s2);
-        System.out.println(s1 == "Hello!".substring(0,5));
-        System.out.println(s1 == "Hello".substring(0,5));
+        System.out.println("Hello" == s1); //true
+        System.out.println(s1 == s2); //false
+        System.out.println(s3 == s2); //false
+        System.out.println(s1 == "Hello!".substring(0,5)); //false
+        System.out.println(s1 == "Hello".substring(0,5)); //true
         
 
     }
