c04491b0b419a95fc36649f0c029a22100bee37f
Dmitriy <altusj@gmail.com>
2023-08-14 13:31:07 -0400

    oca prep

--- oca/StringClass.java
+++ oca/StringClass.java
@@ -3,7 +3,11 @@ package oca;
 public class StringClass {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        String s1 = "Hello";
+        String s2 = new String("Hello");
+        String s3 = new String(new StringBuilder("Hello"));
+        
+        
 
     }
 
