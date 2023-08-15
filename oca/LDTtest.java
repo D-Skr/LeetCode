7fd6162425592456572dbcb77a1dcf48ca882ec9
Dmitriy <altusj@gmail.com>
2023-08-15 11:39:59 -0400

    oca prep

--- oca/LDTtest.java
+++ oca/LDTtest.java
@@ -1,14 +1,20 @@
 package oca;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 public class LDTtest {
 
     public static void main(String[] args) {
         System.out.println(LocalTime.now()); //11:37:02.616
-        System.out.println(LocalDateTime.now()); //11:37:02.616
-
+        System.out.println(LocalDateTime.now()); //2023-08-15T11:37:29.406
+        
+        //RTE: java.time.DateTimeException: Invalid date 'JUNE 31'
+        //System.out.println(LocalDate.of(2020, Month.JUNE, 31));
+        
+        System.out.println(LocalDate.of(2020, Month.JUNE, 29)); //2020-06-29
 
     }
 
