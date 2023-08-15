c92ed8912864427b8cf199d4658367e50e8dd949
Dmitriy <altusj@gmail.com>
2023-08-15 11:37:34 -0400

    oca prep

--- oca/LDTtest.java
+++ oca/LDTtest.java
@@ -1,9 +1,14 @@
 package oca;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class LDTtest {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println(LocalTime.now()); //11:37:02.616
+        System.out.println(LocalDateTime.now()); //11:37:02.616
+
 
     }
 
