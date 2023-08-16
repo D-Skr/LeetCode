c046399c524c2f999d5a02b73f41f6168ab4560d
Dmitriy <altusj@gmail.com>
2023-08-15 20:21:43 -0400

    oca prep

--- oca/ArrListTest.java
+++ oca/ArrListTest.java
@@ -1,9 +1,24 @@
 package oca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrListTest {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        List<Integer> list = new ArrayList<>();
+        list.add(16);
+        list.add(null);
+        
+        for(int i = 0; i < list.size(); i++) System.out.print(list.get(i));
+        for(Integer i : list) System.out.print(i); //prints 16null16null
+        
+        
+        for(int i : list) System.out.print(i);
+        //prints
+        //16nullException in thread "main" 16null16java.lang.NullPointerException
+        //RTE becaus method is unboxing Integer i
+        // public int intValue(null) throws NPE
 
     }
 
