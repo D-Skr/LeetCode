d4965bd4636c4b8efd74407a10309deb17111a15
Dmitriy <altusj@gmail.com>
2023-08-17 17:34:05 -0400

    oca prep

--- oca/StringTest2.java
+++ oca/StringTest2.java
@@ -3,7 +3,13 @@ package oca;
 public class StringTest2 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        final String fName = "James";
+        String lName = "Gosling";
+        String name1 = fName + lName;
+        String name2 = fName + "Gosling";
+        String name3 = "James" + "Gosling";
+        System.out.println(name1 == name2);
+        System.out.println(name2 == name3);
 
     }
 
