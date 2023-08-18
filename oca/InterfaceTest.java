50ffd1fb441c9c409b0aaae326f6dfd56e74449e
Dmitriy <altusj@gmail.com>
2023-08-18 12:04:23 -0400

    oca prep

--- oca/InterfaceTest.java
+++ oca/InterfaceTest.java
@@ -4,10 +4,12 @@ interface I01 {
     void m1();
 }
 
-public class InterfaceTest {
+public class InterfaceTest extends Object implements I01 {
 
+    protected void m1() {} //m1 is public implicitly in I01, should be public here as well, but no comperr
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println(123);
 
     }
 
