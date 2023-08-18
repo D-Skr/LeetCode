2fe4da9689a6ee429964cc6e87aeabbf9217e0da
Dmitriy <altusj@gmail.com>
2023-08-18 00:52:28 -0400

    oca prep

--- oca/SbTest.java
+++ oca/SbTest.java
@@ -7,9 +7,9 @@ public class SbTest {
         System.out.println(sb.length() + " : " + sb.toString().length());
         System.out.println(sb.toString());
         //prints 0 : 0
-        StringBuilder sb2 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder("sb");
 //        System.out.println(sb2.append(null)); //comperr
-        
+        System.out.println(sb2); //prints sb
     }
 
 }
