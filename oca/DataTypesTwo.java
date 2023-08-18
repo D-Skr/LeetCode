602283ffc4b4126f63c7ae78fdb541f075004892
Dmitriy <altusj@gmail.com>
2023-08-18 15:55:46 -0400

    oca prep

--- oca/DataTypesTwo.java
+++ oca/DataTypesTwo.java
@@ -3,7 +3,13 @@ package oca;
 public class DataTypesTwo {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        Integer i1 = 10;
+        Integer i2 = new Integer(i1);
+        
+        //String s; //comperr: The local variable s may not have been initialized
+        String s = null;
+        Boolean b = Boolean.valueOf(s);
+        System.out.println(b);
 
     }
 
