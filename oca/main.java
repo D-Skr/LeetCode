611ab3debe62ee4d5f54ed57e4738cf7696be1bc
Dmitriy <altusj@gmail.com>
2023-08-21 18:31:04 -0400

    oca prep

--- oca/main.java
+++ oca/main.java
@@ -7,7 +7,12 @@ public class main {
         String s1 = "StringS";
         System.out.println(s1.indexOf('S'));
         System.out.println(s1.charAt(s1.indexOf('S')+2));
-        String arr1[], arr2, arr3 = null;
+        String arr1 [], arr2, arr3 = null;
+        arr1 = new String[2];
+        arr1[0] = "a";
+        arr1[1] = "b";
+//        arr2=arr1;
+//        arr3=arr1;
         
     }
 
