52d3b3b4a2bcb8ac47957fe3c1de4fbd0c67dd2a
Dmitriy <altusj@gmail.com>
2023-11-07 11:31:06 -0500

    solution Permuting Two Arrasy

--- arrays/PermutingTwoArrays.java
+++ arrays/PermutingTwoArrays.java
@@ -1,6 +1,17 @@
 package arrays;
 
-    /*
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/*
     * Complete the 'twoArrays' function below.
     *
     * The function is expected to return a STRING.
@@ -10,10 +21,55 @@ package arrays;
     *  3. INTEGER_ARRAY B
     */
 public class PermutingTwoArrays {
+    
+    public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
+        // Write your code here
+        String result = "NO";
+        A.sort(Comparator.naturalOrder());
+        B.sort(Comparator.reverseOrder());
+        for(int i = 0; i < A.size(); i++){
+            if(A.get(i) + B.get(i) >= k) result = "YES";
+            if(A.get(i) + B.get(i) < k){
+                result = "NO";
+                break;
+                }
+            }
+        return result;
+        }
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(String[] args)  throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int k = Integer.parseInt(firstMultipleInput[1]);
+
+                List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+                List<Integer> B = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+                String result = twoArrays(k, A, B);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 
 }
