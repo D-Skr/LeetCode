d2ac746c60068ed5e7a645857e84981cffa8814d
Dmitriy <altusj@gmail.com>
2023-11-07 11:25:21 -0500

    new problem Permuting Two Arrasy

---
+++ arrays/PermutingTwoArrays.java
@@ -0,0 +1,19 @@
+package arrays;
+
+    /*
+    * Complete the 'twoArrays' function below.
+    *
+    * The function is expected to return a STRING.
+    * The function accepts following parameters:
+    *  1. INTEGER k
+    *  2. INTEGER_ARRAY A
+    *  3. INTEGER_ARRAY B
+    */
+public class PermutingTwoArrays {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
