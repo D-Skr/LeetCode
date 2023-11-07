7ed8ac144d95f9480b94867919746156bf47f394
Dmitriy <altusj@gmail.com>
2023-11-07 12:18:08 -0500

    new problem Subarray Division 1

--- arrays/SubarrayDivision1.java
+++ arrays/SubarrayDivision1.java
@@ -1,6 +1,8 @@
 package arrays;
 
-    /*
+import java.util.List;
+
+/*
      * Complete the 'birthday' function below.
      *
      * The function is expected to return an INTEGER.
@@ -11,6 +13,27 @@ package arrays;
      */
 
 public class SubarrayDivision1 {
+    
+    public static int birthday(List<Integer> s, int d, int m) {
+        int result = 0;
+        if(m ==1) {
+            for(Integer i : s){
+                if(i == d) result++;
+            }
+        }
+        if (s.size()< m) return 0;
+        if(m > 1) {
+            for(int i = 0; i < s.size()-m; i++){
+                int sum = s.get(i);
+                while(m > 1){
+                    sum += s.get(i+m-1);
+                    m--;
+                }
+                if(sum == d) result++;
+            }
+        return result;
+        }
+    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
