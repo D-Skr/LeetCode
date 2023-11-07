3162a558383d0cc4e5ee8d2a6afd82f841d8b668
Dmitriy <altusj@gmail.com>
2023-11-07 12:16:20 -0500

    new problem Subarray Division 1

--- arrays/SubarrayDivision1.java
+++ arrays/SubarrayDivision1.java
@@ -1,5 +1,15 @@
 package arrays;
 
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
 public class SubarrayDivision1 {
 
     public static void main(String[] args) {
